d98213303414e56b4f950c2ac73968e52babd098
Nathaniel Tampus <nate@mugnavo.com>
2025-01-29 17:21:42 +0800

    bump deps

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import solid from 'vite-plugin-solid';
-import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  plugins: [solid(), tailwindcss()],
+  plugins: [solid()],
 });
